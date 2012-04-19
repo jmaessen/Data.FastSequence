3871773b7099ebe16dd39e0eb27b7c56051f1c01
Jan-Willem Maessen <jmaessen@alum.mit.edu>
2012-04-19 19:29:54 -0400

    Pending modifications that were being lost from history after mv.

--- src/Data/VectorNode.hs
+++ src/Data/VectorNode.hs
@@ -2,12 +2,13 @@
              DeriveFunctor, DeriveFoldable, DeriveTraversable #-}
 module Data.VectorNode(
     Elem(..), Breadth(..), Size(..), Sized(..),
-    Node, emptyN, singletonN,
-    splitRNode, splitLNode, forceN, consN, snocN,
-    breadth, headN, tailN, initN, lastN, appN, nullN,
-    (!), foldlN', reverseN, fromListN, toListN, adjustN,
-    takeN, dropN
+    Node, empty, singleton,
+    splitR, splitL, force, cons, snoc,
+    breadth, head, tail, init, last, append, null,
+    (!), foldl', reverse, fromList, toList, adjust,
+    take, drop, replicate
     ) where
+import Prelude hiding (head, tail, init, last, take, drop, null, reverse, replicate)
 import Data.Foldable(Foldable(..))
 import Data.Traversable(Traversable(..))
 import qualified Data.Vector as V
@@ -18,7 +19,7 @@ type Breadth = Int
 -- Size describes the count of items held in or beneath a node.
 type Size = Int
 
-data Node a = Node { nodeSize :: !Size, datum :: !(V.Vector a) }
+data Node a = Node {-# UNPACK #-} !Size {-# UNPACK #-} !(V.Vector a)
   deriving (Eq, Functor, Foldable, Traversable)
 
 ------------------------------------------------------------
@@ -45,16 +46,16 @@ instance Sized (Elem a) where
   sizeN (Node _ n) = V.length n
 
 instance Sized (Node a) where
-  size = nodeSize
+  size (Node s _) = s
 
 ------------------------------------------------------------
 -- Node manipulation.
 
-emptyN :: Node a
-emptyN = Node 0 V.empty
+empty :: Node a
+empty = Node 0 V.empty
 
-singletonN :: Elem a -> Node (Elem a)
-singletonN = Node 1 . V.singleton
+singleton :: Elem a -> Node (Elem a)
+singleton = Node 1 . V.singleton
 
 -- Compute size of node the hard way, by summing element sizes.
 
@@ -69,87 +70,91 @@ splitNode :: V.Vector a -> Breadth -> (V.Vector a, V.Vector a)
 splitNode n b = (V.take b n, V.drop b n)
 
 -- Split for R, counting from left, copying left
-{-# SPECIALIZE splitRNode :: Node (Node a) -> Breadth -> (Node (Node a), Node (Node a)) #-}
-{-# SPECIALIZE splitRNode :: Node (Elem a) -> Breadth -> (Node (Elem a), Node (Elem a)) #-}
-splitRNode :: (Sized a) => Node a -> Breadth -> (Node a, Node a)
-splitRNode (Node s n) b =
+{-# SPECIALIZE splitR :: Node (Node a) -> Breadth -> (Node (Node a), Node (Node a)) #-}
+{-# SPECIALIZE splitR :: Node (Elem a) -> Breadth -> (Node (Elem a), Node (Elem a)) #-}
+splitR :: (Sized a) => Node a -> Breadth -> (Node a, Node a)
+splitR (Node s n) b =
   case splitNode n b of
     (d, r) -> (node (V.force d), node r)
 
 -- Split for L, counting from right, copying right
-{-# SPECIALIZE splitLNode :: Node (Elem a) -> Breadth -> (Node (Elem a), Node (Elem a)) #-}
-{-# SPECIALIZE splitLNode :: Node (Node a) -> Breadth -> (Node (Node a), Node (Node a)) #-}
-splitLNode :: (Sized a) => Node a -> Breadth -> (Node a, Node a)
-splitLNode (Node s n) b =
+{-# SPECIALIZE splitL :: Node (Elem a) -> Breadth -> (Node (Elem a), Node (Elem a)) #-}
+{-# SPECIALIZE splitL :: Node (Node a) -> Breadth -> (Node (Node a), Node (Node a)) #-}
+splitL :: (Sized a) => Node a -> Breadth -> (Node a, Node a)
+splitL (Node s n) b =
   case splitNode n (V.length n - b) of
     (l, d) -> (node l, node (V.force d))
 
-forceN :: Node a -> Node a
-forceN (Node s n) = Node s (V.force n)
+force :: Node a -> Node a
+force (Node s n) = Node s (V.force n)
 
-{-# SPECIALIZE consN :: (Elem a) -> Node (Elem a) -> Node (Elem a) #-}
-{-# SPECIALIZE consN :: (Node a) -> Node (Node a) -> Node (Node a) #-}
-consN :: (Sized a) => a -> Node a -> Node a
-consN a (Node s n) = Node (size a + s) (V.cons a n)
+{-# SPECIALIZE cons :: (Elem a) -> Node (Elem a) -> Node (Elem a) #-}
+{-# SPECIALIZE cons :: (Node a) -> Node (Node a) -> Node (Node a) #-}
+cons :: (Sized a) => a -> Node a -> Node a
+cons a (Node s n) = Node (size a + s) (V.cons a n)
 
-{-# SPECIALIZE snocN :: Node (Elem a) -> (Elem a) -> Node (Elem a) #-}
-{-# SPECIALIZE snocN :: Node (Node a) -> (Node a) -> Node (Node a) #-}
-snocN :: (Sized a) => Node a -> a -> Node a
-snocN (Node s n) a = Node (size a + s) (V.snoc n a)
+{-# SPECIALIZE snoc :: Node (Elem a) -> (Elem a) -> Node (Elem a) #-}
+{-# SPECIALIZE snoc :: Node (Node a) -> (Node a) -> Node (Node a) #-}
+snoc :: (Sized a) => Node a -> a -> Node a
+snoc (Node s n) a = Node (size a + s) (V.snoc n a)
 
 breadth :: Node a -> Breadth
 breadth (Node _ n) = V.length n
 
-headN :: Node a -> a
-headN (Node _ n) = V.head n
+head :: Node a -> a
+head (Node _ n) = V.head n
 
-{-# SPECIALIZE tailN :: Node (Elem a) -> Node (Elem a)  #-}
-{-# SPECIALIZE tailN :: Node (Node a) -> Node (Node  a)  #-}
-tailN :: (Sized a) => Node a -> Node a
-tailN (Node s n) = Node (s - size (V.head n)) (V.force (V.tail n))
+{-# SPECIALIZE tail :: Node (Elem a) -> Node (Elem a)  #-}
+{-# SPECIALIZE tail :: Node (Node a) -> Node (Node  a)  #-}
+tail :: (Sized a) => Node a -> Node a
+tail (Node s n) = Node (s - size (V.head n)) (V.force (V.tail n))
 
-lastN :: Node a -> a
-lastN (Node _ n) = V.last n
+last :: Node a -> a
+last (Node _ n) = V.last n
 
-{-# SPECIALIZE initN :: Node (Elem a) -> Node (Elem a)  #-}
-{-# SPECIALIZE initN :: Node (Node a) -> Node (Node  a)  #-}
-initN :: (Sized a) => Node a -> Node a
-initN (Node s n) = Node (s - size (V.last n)) (V.force (V.init n))
+{-# SPECIALIZE init :: Node (Elem a) -> Node (Elem a)  #-}
+{-# SPECIALIZE init :: Node (Node a) -> Node (Node  a)  #-}
+init :: (Sized a) => Node a -> Node a
+init (Node s n) = Node (s - size (V.last n)) (V.force (V.init n))
 
-appN :: Node a -> Node a -> Node a
-appN (Node s1 n1) (Node s2 n2) = Node (s1+s2) (n1 V.++ n2)
+append :: Node a -> Node a -> Node a
+append (Node s1 n1) (Node s2 n2) = Node (s1+s2) (n1 V.++ n2)
 
-nullN :: Node a -> Bool
-nullN (Node s _) = s == 0
+null :: Node a -> Bool
+null (Node s _) = s == 0
 
-foldlN' :: (r -> a -> r) -> r -> Node a -> r
-foldlN' f z (Node _ n) = V.foldl' f z n
+foldl' :: (r -> a -> r) -> r -> Node a -> r
+foldl' f z (Node _ n) = V.foldl' f z n
 
-reverseN :: Node a -> Node a
-reverseN (Node s n) = Node s (V.reverse n)
+reverse :: Node a -> Node a
+reverse (Node s n) = Node s (V.reverse n)
 
-{-# SPECIALIZE fromListN :: [Elem a] -> Node (Elem a)   #-}
-{-# SPECIALIZE fromListN :: [Node a] -> Node (Node a)   #-}
-fromListN :: (Sized a) => [a] -> Node a
-fromListN xs = node (V.fromList xs)
+{-# SPECIALIZE fromList :: [Elem a] -> Node (Elem a)   #-}
+{-# SPECIALIZE fromList :: [Node a] -> Node (Node a)   #-}
+fromList :: (Sized a) => [a] -> Node a
+fromList xs = node (V.fromList xs)
 
-toListN :: Node a -> [a]
-toListN (Node _ v) = V.toList v
+toList :: Node a -> [a]
+toList (Node _ v) = V.toList v
 
-{-# INLINE adjustN #-}
+{-# INLINE adjust #-}
 -- Note that lack of Sized constraint forces this to be a
 -- size-preserving adjustment.
-adjustN :: (a -> a) -> Breadth -> Node a -> Node a
-adjustN f i (Node s v) = Node s (v V.// [(i, f (v V.! i))])
+adjust :: (a -> a) -> Breadth -> Node a -> Node a
+adjust f i (Node s v) = Node s (v V.// [(i, f (v V.! i))])
 
 {-# INLINE (!) #-}
 (!) :: Node a -> Breadth -> a
 (!) (Node s n) i = n V.! i
 
 -- take without a Sized constraint (caller adjusts the size)
-takeN :: Size -> Breadth -> Node a -> Node a
-takeN s i (Node _ v) = Node s (V.take i v)
+take :: Size -> Breadth -> Node a -> Node a
+take s i (Node _ v) = Node s (V.take i v)
 
 -- drop without a Sized constraint (caller adjusts the size)
-dropN :: Size -> Breadth -> Node a -> Node a
-dropN s i (Node _ v) = Node s (V.drop i v)
+drop :: Size -> Breadth -> Node a -> Node a
+drop s i (Node _ v) = Node s (V.drop i v)
+
+-- replication
+replicate :: Size -> Breadth -> a -> Node a
+replicate s i a = Node s (V.replicate i a)
